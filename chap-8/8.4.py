ef81e7faa902b5944a1e8553fe12055b0e1d3889
唐树森 <14021051@buaa.edu.cn>
2018-04-10 09:58:40 +0800

    add chap-8

--- chap-8/8.4.py
+++ chap-8/8.4.py
@@ -13,8 +13,12 @@ TRAINING_EXAMPLES = 10000                   # 训练数据个数。
 TESTING_EXAMPLES = 1000                     # 测试数据个数。
 SAMPLE_GAP = 0.01                           # 采样间隔
 
-# 产生数据
+
 def generate_data(seq):
+    '''
+    将序列数据整理成feed进RNN的数据
+    :param seq: sin序列
+    '''
     X = []
     y = []
     # 序列的第i项和后面的TIMESTEPS-1项合在一起作为输入；第i + TIMESTEPS项作为输
@@ -25,17 +29,18 @@ def generate_data(seq):
     return np.array(X, dtype=np.float32), np.array(y, dtype=np.float32) # X.shape = (10000, 1, 10), y.shape = (10000, 1)
 
 
-# 用正弦函数生成训练和测试数据集合。
-test_start = (TRAINING_EXAMPLES + TIMESTEPS) * SAMPLE_GAP
-test_end = test_start + (TESTING_EXAMPLES + TIMESTEPS) * SAMPLE_GAP
-# linspace(start,end,num)创建一个长度为num的等差数列
-train_X, train_y = generate_data(np.sin(np.linspace(
-    0, test_start, TRAINING_EXAMPLES + TIMESTEPS, dtype=np.float32)))
-# print(train_y.shape)
-# print(train_X.shape)
-test_X, test_y = generate_data(np.sin(np.linspace(
-    test_start, test_end, TESTING_EXAMPLES + TIMESTEPS, dtype=np.float32)))
-
+def data():
+    # 用正弦函数生成训练和测试数据集合
+    test_start = (TRAINING_EXAMPLES + TIMESTEPS) * SAMPLE_GAP
+    test_end = test_start + (TESTING_EXAMPLES + TIMESTEPS) * SAMPLE_GAP
+    # linspace(start,end,num)创建一个长度为num的等差数列
+    train_X, train_y = generate_data(np.sin(np.linspace(
+        0, test_start, TRAINING_EXAMPLES + TIMESTEPS, dtype=np.float32)))
+    # print(train_y.shape)
+    # print(train_X.shape)
+    test_X, test_y = generate_data(np.sin(np.linspace(
+        test_start, test_end, TESTING_EXAMPLES + TIMESTEPS, dtype=np.float32)))
+    return train_X, train_y, test_X, test_y
 
 
 # 定义网络结构和优化步骤
@@ -110,30 +115,40 @@ def run_eval(sess, test_X, test_y):
     plt.show()
 
 
-# 执行训练并测试
-# 将训练数据以数据集的方式提供给计算图。
-ds = tf.data.Dataset.from_tensor_slices((train_X, train_y))
-ds = ds.repeat().shuffle(1000).batch(BATCH_SIZE)
-X, y = ds.make_one_shot_iterator().get_next()
-print(X.shape)
+# 训练
+def train(train_X, train_y, test_X, test_y):
+    # 将训练数据以数据集的方式提供给计算图。
+    ds = tf.data.Dataset.from_tensor_slices((train_X, train_y))
+    ds = ds.repeat().shuffle(1000).batch(BATCH_SIZE)
+    X, y = ds.make_one_shot_iterator().get_next()
+    print(X.shape)
 
-# 定义模型，得到预测结果、损失函数，和训练操作。
-with tf.variable_scope("model"):
-    _, loss, train_op = lstm_model(X, y, True)
+    # 定义模型，得到预测结果、损失函数，和训练操作。
+    with tf.variable_scope("model"):
+        _, loss, train_op = lstm_model(X, y, True)
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-    # 测试在训练之前的模型效果。
-    print("训练前均方误差："),
-    run_eval(sess, test_X, test_y)
+        # 测试在训练之前的模型效果。
+        print("训练前均方误差："),
+        run_eval(sess, test_X, test_y)
 
-    # 训练模型。
-    for i in range(TRAINING_STEPS):
-        _, l = sess.run([train_op, loss])
-        if i % 1000 == 0:
-            print("train step: " + str(i) + ", loss: " + str(l))
+        # 训练模型。
+        for i in range(TRAINING_STEPS):
+            _, l = sess.run([train_op, loss])
+            if i % 1000 == 0:
+                print("train step: " + str(i) + ", loss: " + str(l))
 
-    # 使用训练好的模型对测试数据进行预测。
-    print("\n训练后均方误差："),
-    run_eval(sess, test_X, test_y)
+        # 使用训练好的模型对测试数据进行预测。
+        print("\n训练后均方误差："),
+        run_eval(sess, test_X, test_y)
+
+
+def main():
+    train_X, train_y, test_X, test_y = data()
+    train(train_X, train_y, test_X, test_y)
+
+
+if __name__ == '__main__':
+    main()
